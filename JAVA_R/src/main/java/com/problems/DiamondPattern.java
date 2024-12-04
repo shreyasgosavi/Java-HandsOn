968987b92d6c36f837a2714993e9b999f03415fa
shreyasgosavi <shreyasgosavi2016@gmail.com>
2024-12-04 22:18:20 +0530

    Based on problem

--- JAVA_R/src/main/java/com/problems/DiamondPattern.java
+++ JAVA_R/src/main/java/com/problems/DiamondPattern.java
@@ -1,71 +1,148 @@
 package com.problems;
 
-import java.util.Scanner;
+import java.util.ArrayList;
 
-public class DiamondPattern {
+import java.util.List;
+
+class DiamondPattern {
 
     static int aASCII = 'A';
     static int inputCharacter;
     static int gap;
     static int spaceBetween = 0;
     static boolean isFinished = false;
-
     static boolean firstHalf = true;
+    List<String> printToList(char a) {
 
-    public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-
-        inputCharacter = sc.next().toUpperCase().charAt(0);
+        inputCharacter = a;
         gap = inputCharacter - aASCII;
+        ArrayList<String> rowLines = new ArrayList<>();
 
         while (!isFinished) {
 
+            StringBuilder sb = new StringBuilder();
+
             for (int i = 0; i < gap; i++) {
                 System.out.print(" ");
+                sb.append(" ");
             }
             System.out.print((char) aASCII);
+            sb.append((char) aASCII);
 
             if (aASCII != 'A') {
                 for (int i = 0; i < spaceBetween; i++) {
                     System.out.print(" ");
+                    sb.append(" ");
                 }
                 System.out.print((char) aASCII);
+                sb.append((char) aASCII);
             }
             for (int i = 0; i < gap; i++) {
                 System.out.print(" ");
+                sb.append(" ");
             }
 
             System.out.println();
-
+            rowLines.add(sb.toString());
             getUpdatedProperties();
         }
-
-
-
-
+        return rowLines;
     }
 
-    public static void getUpdatedProperties(){
+    public static void getUpdatedProperties() {
 
-        if(aASCII == inputCharacter){
+        if (aASCII == inputCharacter) {
             firstHalf = false;
         }
 
-        if(aASCII == 'A' && !firstHalf){
-            isFinished=true;
+        if (aASCII == 'A' && !firstHalf) {
+            isFinished = true;
         }
 
-        if(firstHalf){
+        if (firstHalf) {
             spaceBetween += 2;
             gap--;
             aASCII++;
-        }
-        else{
+        } else {
             aASCII--;
             spaceBetween -= 2;
             gap++;
         }
 
     }
+
+    public static void main(String[] args) {
+
+        DiamondPattern diamondPattern = new DiamondPattern();
+        List<String> string = diamondPattern.printToList('D');
+
+        System.out.println(string);
+    }
 }
+
+
+//public class DiamondPattern {
+//
+//    static int aASCII = 'A';
+//    static int inputCharacter;
+//    static int gap;
+//    static int spaceBetween = 0;
+//    static boolean isFinished = false;
+//
+//    static boolean firstHalf = true;
+//
+//    public static void main(String[] args) {
+//
+//        Scanner sc = new Scanner(System.in);
+//
+//        inputCharacter = sc.next().toUpperCase().charAt(0);
+//        gap = inputCharacter - aASCII;
+//
+//        while (!isFinished) {
+//
+//            for (int i = 0; i < gap; i++) {
+//                System.out.print(" ");
+//            }
+//            System.out.print((char) aASCII);
+//
+//            if (aASCII != 'A') {
+//                for (int i = 0; i < spaceBetween; i++) {
+//                    System.out.print(" ");
+//                }
+//                System.out.print((char) aASCII);
+//            }
+//            for (int i = 0; i < gap; i++) {
+//                System.out.print(" ");
+//            }
+//
+//            System.out.println();
+//
+//            getUpdatedProperties();
+//        }
+//
+//
+//    }
+//
+//    public static void getUpdatedProperties() {
+//
+//        if (aASCII == inputCharacter) {
+//            firstHalf = false;
+//        }
+//
+//        if (aASCII == 'A' && !firstHalf) {
+//            isFinished = true;
+//        }
+//
+//        if (firstHalf) {
+//            spaceBetween += 2;
+//            gap--;
+//            aASCII++;
+//        } else {
+//            aASCII--;
+//            spaceBetween -= 2;
+//            gap++;
+//        }
+//
+//    }
+//}
